9742a06379f40c794bbc8ce471a2a9a92df15ad7
yaseenahmed02 <yaseen@aucegypt.edu>
2023-10-28 21:20:36 +0200

    developed navigation:

--- disk-analyzer/src/App.jsx
+++ disk-analyzer/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Summary from "./summary";
 import Home from "./home";
-import Progress from "./progress"
+import Progress from "./progress";
 import "./styles.css";
 
 function App() {
   return (
-    <div className="h-screen">
-      {<Home />}
-      {/* <Progress/> */}
-      {/*<Summary /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/progress" element={<Progress />} />
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
+    </Router>
   );
 }
 
--- disk-analyzer/src/progress.jsx
+++ disk-analyzer/src/progress.jsx
@@ -1,12 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Progress() {
-  const [progress, setProgress] = useState(60);
+  const [progress, setProgress] = useState(0);
+  const [message, setMessage] = useState("Initializing...");
+  const navigate = useNavigate(); // Hook to programmatically navigate
 
-  // Function to update the progress
-  const updateProgress = (newProgress) => {
-    setProgress(newProgress);
-  };
+  useEffect(() => {
+    if (progress < 100) {
+      const interval = setInterval(() => {
+        const newProgress = progress + 10;
+        setProgress(newProgress);
+
+        switch (newProgress) {
+          case 10:
+            setMessage("Accessing memory...");
+            break;
+          case 30:
+            setMessage("Scanning disk...");
+            break;
+          case 60:
+            setMessage("Analyzing data blocks...");
+            break;
+          case 90:
+            setMessage("Finalizing scan...");
+            break;
+          case 100:
+            setMessage("Scan complete!");
+            clearInterval(interval); // Clear the interval when scan is complete
+            navigate("/summary"); // Navigate to the summary page
+            break;
+          default:
+            break;
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [progress, navigate]);
 
   return (
     <div className="Progress">
@@ -17,10 +50,10 @@ function Progress() {
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <p>Currently viewing in memory...</p>
+      <p>{message}</p>
       <div>
-        <button onClick={() => updateProgress(30)}>Pause</button>
-        <button onClick={() => updateProgress(0)}>Cancel</button>
+        <button onClick={() => setProgress(progress - 10)}>Pause</button>
+        <button onClick={() => setProgress(0)}>Cancel</button>
       </div>
     </div>
   );
